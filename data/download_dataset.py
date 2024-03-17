77bfb407c2bac1dc70a58bf2a4011e027082de37
MrIceHavoc <afonso.fernandes100@gmail.com>
2024-03-17 19:39:40 +0000

    Added dataset to gitignore and function to download from gdrive

---
+++ data/download_dataset.py
@@ -0,0 +1,28 @@
+import requests
+
+def download_file_from_gdrive(file_id, dest):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+        return None
+    
+    def save_response_content(response, destination):
+        CHUNK_SIZE = 32768
+        with open(destination, "wb") as f:
+            for chunk in response.iter_content(CHUNK_SIZE):
+                if chunk:  # filter out keep-alive new chunks
+                    f.write(chunk)
+                    
+    URL = f"https://drive.google.com/file/d/{file_id}/view?usp=drive_link"
+
+    session = requests.Session()
+
+    resp = session.get(url=URL, params={'id': file_id}, stream=True)
+    token = get_confirm_token(response=resp)
+
+    if token:
+        params = {'id': file_id, 'confirm': token}
+        resp = session.get(url=URL, params=params, stream=True)
+
+    save_response_content(response=resp, destination=dest)
